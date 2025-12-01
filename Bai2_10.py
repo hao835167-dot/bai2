6407bb25732b7370ea7db8e776eed0be583e668f
hao835167-dot <hao835167@gmail.com>
2025-12-01 08:37:22 +0700

    Update Bai2_10.py

--- Bai2_10.py
+++ Bai2_10.py
@@ -1,3 +1,7 @@
+print("Tên: trần văn hào")
+print("Msv:245752021610153")
+print("#############################")
+######################################
 s = input("Nhap chuoi: ")
 words = s.split()
 print("Sau khi split:", words)
