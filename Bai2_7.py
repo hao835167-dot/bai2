dc5da2c1b02d333dbcd25b42a6d9a7376fafe948
hao835167-dot <hao835167@gmail.com>
2025-12-01 08:39:40 +0700

    Add student information and square dictionary

--- Bai2_7.py
+++ Bai2_7.py
@@ -1,3 +1,7 @@
+print("Tên: trần văn hào")
+print("Msv:245752021610153")
+print("#############################")
+######################################
 n=int(input("nhap vao mot so nguyen:"))
 d=dict()
 for i in range (1,n+1):
