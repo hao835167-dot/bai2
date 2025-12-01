86ef1bd77a64d89a86d045346ffc63a49e39e14e
hao835167-dot <hao835167@gmail.com>
2025-12-01 08:39:09 +0700

    Update Bai2_5.py

--- Bai2_5.py
+++ Bai2_5.py
@@ -1,3 +1,7 @@
+print("Tên: trần văn hào")
+print("Msv:245752021610153")
+print("#############################")
+######################################
 n = int(input("enter a number (n>0):"))
 while n >= 0:
     print(n)
