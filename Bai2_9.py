fd08afabe1a2deb694626aa526cd8da32918df41
hao835167-dot <hao835167@gmail.com>
2025-12-01 08:40:15 +0700

    Update Bai2_9.py

--- Bai2_9.py
+++ Bai2_9.py
@@ -1,3 +1,7 @@
+print("Tên: trần văn hào")
+print("Msv:245752021610153")
+print("#############################")
+######################################
 s = input("Nhap chuoi: ")
 result = {}
 for char in s:
